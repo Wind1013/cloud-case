3438fc9d9e7c9be456c57ef1e18d857414e540dd
Wind1013 <justinariem@gmail.com>
2025-09-14 19:31:19 +0800

    fix trial on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  runtime: "nodejs",
+  // runtime: "nodejs",
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up).*)",
   ],
