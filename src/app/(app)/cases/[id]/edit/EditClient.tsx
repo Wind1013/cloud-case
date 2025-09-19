4b50256382f56d2d5f3eb7b7f6d6bc3eb76e0cb6
Wind1013 <justinariem@gmail.com>
2025-09-19 21:47:42 +0800

    fix typescript errors on case edit files

--- src/app/(app)/cases/[id]/edit/EditClient.tsx
+++ src/app/(app)/cases/[id]/edit/EditClient.tsx
@@ -10,15 +10,13 @@ import { useRouter } from "next/navigation";
 
 function EditClient({
   clients,
-  lawyers,
   caseData,
 }: {
   clients: User[];
-  lawyers: User[];
   caseData: Case;
 }) {
   const router = useRouter();
-  const { description, title, lawyerId, clientId, id } = caseData;
+  const { description, title, clientId, id } = caseData;
 
   const onSubmit = async (data: CaseFormValues) => {
     try {
@@ -38,10 +36,8 @@ function EditClient({
     <div className="max-w-7xl mx-auto p-8 rounded-xl shadow-lg bg-gray-50 border border-gray-200 mt-10">
       <CaseFilingForm
         clients={clients}
-        lawyers={lawyers}
         onSubmit={onSubmit}
         initialData={{
-          lawyerId,
           clientId,
           description: description || "",
           title,
--- src/app/(app)/cases/[id]/edit/page.tsx
+++ src/app/(app)/cases/[id]/edit/page.tsx
@@ -1,20 +1,19 @@
-import { getCaseById } from "@/actions/cases";
-import { getUsers } from "@/actions/users";
 import React from "react";
 import EditClient from "./EditClient";
 import { notFound } from "next/navigation";
+import { getUsers } from "@/data/users";
+import { getCaseById } from "@/data/cases";
 
 async function EditPage({ params }: { params: { id: string } }) {
   const { id } = await params;
   const clients = await getUsers({ role: "CLIENT" });
-  const lawyers = await getUsers({ role: "LAWYER" });
   const { data } = await getCaseById(id);
 
   if (!data) {
     notFound();
   }
 
-  return <EditClient clients={clients} lawyers={lawyers} caseData={data} />;
+  return <EditClient clients={clients} caseData={data} />;
 }
 
 export default EditPage;
