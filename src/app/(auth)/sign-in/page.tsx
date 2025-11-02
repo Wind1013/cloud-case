a85008efcee5c34059354d7557db1c1ab3aaa49b
Wind1013 <justinariem@gmail.com>
2025-11-02 20:57:50 +0800

    changed UI on sign-in and global colors

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,17 +1,21 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
 import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+        <div className="flex flex-col items-center gap-3 self-center w-full">
+          <div className="relative w-full aspect-[3/2]">
+            <Image
+              fill
+              src="/logo.png"
+              alt="Atty. Michael B. Bongalonta Law Office"
+              className="object-contain"
+              priority
+            />
           </div>
-          Law Office
-        </a>
+        </div>
         <LoginForm />
       </div>
     </div>
